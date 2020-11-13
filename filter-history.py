d8238ce60ee211411fece080154e92a36ee029e0
Konstantin Osipov <kostja@scylladb.com>
2020-11-13 13:51:24 +0300

    Make key optional.

--- filter-history.py
+++ filter-history.py
@@ -5,18 +5,25 @@ import sys
 
 def parse_cmd_line():
     parser = argparse.ArgumentParser(description="history.edn filter")
+
     parser.add_argument(
         "path",
         nargs = "*",
         action="store",
         help="""Path to history.edn""")
+
+    parser.add_argument(
+        '--key', '-k', action="store",
+        default="1", type=str,
+        help="The key to look for")
+
     args = parser.parse_args()
     return args
 
-def match(line):
-    if re.search(":append 1 ", line):
+def match(line, key):
+    if re.search(":append {} ".format(key), line):
         return True
-    if re.search(":r 1 ", line):
+    if re.search(":r {} ".format(key), line):
         return True
     if re.search(":r \d+ ", line):
         return False
@@ -31,7 +38,7 @@ def main():
             l = f.readline()
             if not l:
                 break
-            if match(l):
+            if match(l, args.key):
                 sys.stdout.write(l)
 
 if __name__ == "__main__":
